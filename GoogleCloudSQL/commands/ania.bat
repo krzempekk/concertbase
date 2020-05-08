0e9952e90577e2a5fd6ea0ae7ca316c79ebe106a
GabenRulez <45923220+GabenRulez@users.noreply.github.com>
2020-05-08 21:02:22 +0200

    Added proxy connection files (Google CloudSQL). Added 2 .bat files to run proxy.

---
+++ GoogleCloudSQL/commands/ania.bat
@@ -0,0 +1,2 @@
+"../cloud_sql_proxy_win64.exe" -instances="decent-seeker-276616:europe-west1:concert-base-sql"=tcp:8080 -credential_file="../connectionFiles/decent-seeker-276616-cc9d375f12a0_ania.json"
+pause
