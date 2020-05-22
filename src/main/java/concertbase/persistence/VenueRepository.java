cd014ac637d3d82a621673d5da1323d1b1f34df4
enkelian <aniano13@gmail.com>
2020-05-22 20:03:56 +0200

    adding added

--- src/main/java/concertbase/persistence/VenueRepository.java
+++ src/main/java/concertbase/persistence/VenueRepository.java
@@ -3,5 +3,8 @@ package concertbase.persistence;
 import concertbase.model.Venue;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface VenueRepository extends CrudRepository<Venue, Long> {
+    List<Venue> findByNameAndCity(String name, String city);
 }
--- src/main/java/concertbase/service/VenueService.java
+++ src/main/java/concertbase/service/VenueService.java
@@ -1,5 +1,8 @@
 package concertbase.service;
 
+import concertbase.model.Venue;
+import concertbase.persistence.VenueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,8 +10,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class VenueService {
 
-//    @Autowired
-//    VenueRepository venueRepository;
+    @Autowired
+    VenueRepository venueRepository;
 
 
+    public void addVenue(String name, String country, String city, String street, int streetNumber, String zipCode){
+        this.venueRepository.save(new Venue(name, country, city, street, streetNumber, zipCode));
+    }
 }
