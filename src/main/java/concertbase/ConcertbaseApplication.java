716286e9c5a8152c48fb0f2405c779a20e3c3313
enkelian <aniano13@gmail.com>
2020-05-22 16:37:08 +0200

    subgenre

--- src/main/java/concertbase/ConcertbaseApplication.java
+++ src/main/java/concertbase/ConcertbaseApplication.java
@@ -1,16 +1,22 @@
 package concertbase;
 
 import concertbase.model.Genre;
+import concertbase.model.Subgenre;
 import concertbase.persistence.GenreRepository;
+import concertbase.persistence.SubgenreRespository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = { "concertbase.persistence" })
+@EntityScan(basePackages = { "concertbase.model" })
 public class ConcertbaseApplication {
 	private static final Logger log = LoggerFactory.getLogger(ConcertbaseApplication.class);
 
@@ -32,19 +38,28 @@ public class ConcertbaseApplication {
 //	}
 
 	@Bean
-	public CommandLineRunner demo(GenreRepository repository) {
+	public CommandLineRunner demo(SubgenreRespository repository, GenreRepository genreRepository) {
 		return args -> {
-			repository.save(new Genre("Black metal"));
-			repository.save(new Genre("Death metal"));
-			repository.save(new Genre("Melodic death metal"));
-			repository.save(new Genre("Vegetarian progressive grindcore"));
 
-			log.info("Genres found using findAll():");
-			log.info("-------------------------------");
-			for (Genre genre : repository.findAll()) {
-				log.info(genre.getName());
-			}
-			System.out.println(repository.findByName("Black metal").get(0).getName());
+			genreRepository.save(new Genre("Black metal"));
+//			repository.save(new Genre("Death metal"));
+//			repository.save(new Genre("Melodic death metal"));
+//			repository.save(new Genre("Vegetarian progressive grindcore"));
+//
+//			log.info("Genres found using findAll():");
+//			log.info("-------------------------------");
+			Genre genreFound = genreRepository.findByName("Black metal").get(0);
+			System.out.println(genreFound.getName());
+
+			Subgenre subgenre = new Subgenre("Post");
+			repository.save(subgenre);
+
+//			subgenre.setGenre(genreFound);
+//			for (Genre genre : genreRepository.findAll()) {
+//				System.out.println(genre.getName());
+//			}
+
+//			System.out.println(genreRepository.findByName("Black metal").get(0).getName());
 		};
 	}
 }
