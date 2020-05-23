e1b349cba141882a6ae7fe70723a7fdfdc9b6c4c
GabenRulez (PC Krosno) <wojtex1221@gmail.com>
2020-05-24 01:20:15 +0200

    Dodany nowy mockup

--- src/main/java/concertbase/DataLoader.java
+++ src/main/java/concertbase/DataLoader.java
@@ -32,12 +32,14 @@ public class DataLoader implements ApplicationRunner {
         venueService.addVenue("Zaścianek","Kraków","Stanisława Skarżyńskiego", 1, "31-866");
         venueService.addVenue("Progresja","Warszawa","Fort Wola", 22, "01-258");
         venueService.addVenue("Spodek","Katowice","aleja Korfantego", 35, "40-005");
+        venueService.addVenue("Ogródek Andrzeja", "Strzyżów", "ul. Matematyka", 17, "69-420");
 
 
         genreService.addGenre("metal");
         genreService.addGenre("rock");
         genreService.addGenre("jazz");
         genreService.addGenre("pop");
+        genreService.addGenre("bumbum");
 
 
         genreService.addSubgenre("death metal", "metal");
@@ -47,6 +49,7 @@ public class DataLoader implements ApplicationRunner {
         genreService.addSubgenre("progressive rock", "rock");
         genreService.addSubgenre("acoustic rock", "rock");
         genreService.addSubgenre("acoustic pop", "pop");
+        genreService.addSubgenre("bumbum taptap", "bumbum");
 
 
         artistService.addArtist("Lamb of God", "groove metal");
@@ -55,21 +58,24 @@ public class DataLoader implements ApplicationRunner {
         artistService.addArtist("Bloodbath", "death metal");
         artistService.addArtist("Ayreon", "progressive rock");
         artistService.addArtist("Lucy Rose", "acoustic pop");
+        artistService.addArtist("Andrzej", "bumbum taptap");
 
 
         Concert concert1 = concertService.addLiveConcert("State of unrest tour", "2021-04-02", "https://www.knockoutprod.net/", "Studio", "Kraków");
         Concert concert2 =  concertService.addLiveConcert("Ayreon Universe", "2021-01-20", "https://www.knockoutprod.net/", "Progresja", "Warszawa");
         Concert concert3 = concertService.addLiveConcert("Bloodbath world tour", "2021-03-03", "https://www.knockoutprod.net/", "Spodek", "Katowice");
-
+        Concert concert6 = concertService.addLiveConcert("Andrzej śpiewa i wiruje", "2020-06-22", "https://www.strzyzow.pl/", "Ogródek Andrzeja", "Strzyżów");
 
         Concert concert4 = concertService.addStreamedConcert("Lucy Rose stream", "2020-07-03", "https://www.knockoutprod.net/", "https://www.facebook.com/");
         Concert concert5 = concertService.addStreamedConcert("Panihida live", "2020-06-06", "https://www.knockoutprod.net/", "https://www.facebook.com/");
+
 
 
         artistService.addPerformance("Kreator", concert1.getId());
         artistService.addPerformance("Lamb of God", concert1.getId());
         artistService.addPerformance("Ayreon", concert2.getId());
         artistService.addPerformance("Bloodbath", concert3.getId());
+        artistService.addPerformance("Andrzej", concert6.getId());
 
         artistService.addPerformance("Lucy Rose", concert4.getId());
         artistService.addPerformance("Batushka", concert5.getId());
